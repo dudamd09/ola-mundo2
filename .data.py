b81e2683e5fd3c691b9ed54cc3724c2ab2983d1e
dudamd09 <126122487+dudamd09@users.noreply.github.com>
2023-03-08 23:42:06 +0000

    mudei o arquivo ex

---
+++ .data.py
@@ -0,0 +1,4 @@
+variavel_dia= input("escreva o dia:  ")
+variavel_mês= input("escreva o mês:  ")
+variavel_ano= input("escreva o ano:  ")
+print(f {variavel_dia}, {variavel_mês}, {variavel_ano},sep=/)
---
+++ ex02.py
@@ -0,0 +1,3 @@
+A=(int(input("digite um numero")))
+B=(int(input("sigite um numero")))
+print(A+B)
