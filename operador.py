db16f22c80434ab34bb8bc32e5e911c537d82534
dudamd09 <126122487+dudamd09@users.noreply.github.com>
2023-03-08 20:49:02 +0000

    criei alterações no operador py

--- operador.py
+++ operador.py
@@ -1,4 +1,4 @@
-variavel_nome = input("escreva seu nome:   ")
-print("meu nome é + variavel_nome")
-variavel_idade = input("escreva a idade:  ")
-print("o(a)" + variavel_nome +" possui " + variavel_idade + " anos ")
+A = input("escreva seu nome:   ")
+B = input("escreva sua profissão:  ")
+C = input ("por fim, adicione o endereço: ")
+print("nome: " + A + " profissão: " + B + " endereço: " + C  )
